c99e85db230d428c606d66615b02fbfc25b72fb4
Phelype Rodrigo <81370546+phelype-dev@users.noreply.github.com>
2025-06-26 01:10:48 -0300

    button

--- src/pages/Cadastros/Coletores/index.tsx
+++ src/pages/Cadastros/Coletores/index.tsx
@@ -11,7 +11,7 @@ import {
 import { CadastroContainer } from "../../../components/Main/CadastroContainer";
 
 //Importação de icones Lucid.
-import { CirclePlus, Trash2 } from "lucide-react";
+import { CirclePlus, Pencil, Trash2 } from "lucide-react";
 import { useState } from "react";
 import { CustomDrawer } from "../../../components/Drawer/DrawerComponent";
 import { FormCadColetores } from "./FormCadastroColetores";
@@ -21,12 +21,30 @@ export function CadastroColetoresPage() {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
   const [buttonDelete, setButtonDelete] = useState<boolean>(true);
+  const [buttonEditing, setButtonEditing] = useState<boolean>(true);
 
   const handleButton = () => setIsOpen(true);
 
   const handleSelecionar = (id: string) => {
     setSelectedItem(id === selectedItem ? null : id);
     setButtonDelete(id === selectedItem);
+    setButtonEditing(id === selectedItem);
+  };
+
+  const hanldleDelete = () => {
+    if (selectedItem) {
+      // Aqui você pode implementar a lógica de exclusão do item selecionado
+      alert(`Deletar item com ID: ${selectedItem}`);
+      // Exemplo: dadosColetores = dadosColetores.filter(item => item.id !== selected}
+    }
+  };
+
+  const handleEditar = () => {
+    if (selectedItem) {
+      // Aqui você pode implementar a lógica de edição do item selecionado
+      alert(`Editar item com ID: ${selectedItem}`);
+      // Exemplo: abrir um modal ou redirecionar para uma página de edição
+    }
   };
 
   return (
@@ -36,7 +54,7 @@ export function CadastroColetoresPage() {
         <div className="flex flex-row justify-end p-3">
           <Button
             color="none"
-            className="mr-1 gap-1 rounded-lg px-4 py-2 text-xl font-medium text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 focus:outline-none md:mr-2 md:px-5 md:py-2.5 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-red-800"
+            className="mr-1 cursor-pointer gap-1 rounded-lg px-4 py-2 text-xl font-medium text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 focus:outline-none md:mr-2 md:px-5 md:py-2.5 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-red-800"
             size="xs"
             onClick={handleButton}
             disabled={!buttonDelete}
@@ -46,11 +64,23 @@ export function CadastroColetoresPage() {
           </Button>
 
           {/*Renderização condicional, ao clicar no checkbox, será habilidado o button*/}
+          {!buttonEditing && (
+            <Button
+              color="none"
+              className="mr-1 cursor-pointer gap-1 rounded-lg px-4 py-2 text-xl font-medium text-gray-800 hover:bg-red-400 focus:ring-4 focus:ring-red-300 focus:outline-none md:mr-2 md:px-5 md:py-2.5 dark:text-white dark:hover:bg-green-400 dark:focus:ring-green-800"
+              size="xs"
+              onClick={handleEditar}
+            >
+              <Pencil className="align-middle" size={18} />
+              <span className="text-[16px]">Editar</span>
+            </Button>
+          )}
           {!buttonDelete && (
             <Button
               color="none"
-              className="mr-1 gap-1 rounded-lg px-4 py-2 text-xl font-medium text-gray-800 hover:bg-red-400 focus:ring-4 focus:ring-red-300 focus:outline-none md:mr-2 md:px-5 md:py-2.5 dark:text-white dark:hover:bg-red-700 dark:focus:ring-gray-800"
+              className="mr-1 cursor-pointer gap-1 rounded-lg px-4 py-2 text-xl font-medium text-gray-800 hover:bg-red-400 focus:ring-4 focus:ring-red-300 focus:outline-none md:mr-2 md:px-5 md:py-2.5 dark:text-white dark:hover:bg-red-700 dark:focus:ring-gray-800"
               size="xs"
+              onClick={hanldleDelete}
             >
               <Trash2 className="align-middle" size={18} />
               <span className="text-[16px]">Deletar</span>
@@ -60,14 +90,26 @@ export function CadastroColetoresPage() {
 
         <Table hoverable>
           <TableHead>
-            <TableHeadCell className="p-4 size-0.5">
-              <Checkbox disabled />
-            </TableHeadCell>
-            <TableHeadCell className="text-gray-100 font-bold">ID</TableHeadCell>
-            <TableHeadCell className="text-gray-100 font-bold">Cidade</TableHeadCell>
-            <TableHeadCell className="text-gray-100 font-bold">Cód Coletor</TableHeadCell>
-            <TableHeadCell className="text-gray-100 font-bold">Serial</TableHeadCell>
-            <TableHeadCell className="text-gray-100 font-bold">Executor</TableHeadCell>
+            <TableRow>
+              <TableHeadCell className="size-0.5 p-4">
+                <Checkbox disabled />
+              </TableHeadCell>
+              <TableHeadCell className="font-bold text-gray-100">
+                ID
+              </TableHeadCell>
+              <TableHeadCell className="font-bold text-gray-100">
+                Cidade
+              </TableHeadCell>
+              <TableHeadCell className="font-bold text-gray-100">
+                Cód Coletor
+              </TableHeadCell>
+              <TableHeadCell className="font-bold text-gray-100">
+                Serial
+              </TableHeadCell>
+              <TableHeadCell className="font-bold text-gray-100">
+                Executor
+              </TableHeadCell>
+            </TableRow>
           </TableHead>
           <TableBody className="divide-y">
             {dadosColetores.map((dado, index) => (
